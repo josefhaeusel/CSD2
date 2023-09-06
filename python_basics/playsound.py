a6cf2e7946395687224c21783adb884b82336288
Josef Haeusel <josef.haeusel@yahoo.de>
2023-09-06 17:50:58 +0200

    added playsound.py

---
+++ python_basics/playsound.py
@@ -0,0 +1,11 @@
+import simpleaudio as sa
+
+filepath = input("input path to .wav file")
+wave_obj = sa.WaveObject.from_wave_file(filepath)
+play_obj = wave_obj.play()
+play_obj.wait_done()
+
+n_times = input("How often do you want the file to play (int)?")
+
+for x in range(n_times):
+    play_obj.wait_done()
