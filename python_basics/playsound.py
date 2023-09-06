66ae93820f16dfc23e1f71799913c84cfd9e6e9b
Josef Haeusel <josef.haeusel@yahoo.de>
2023-09-06 17:53:24 +0200

    fixed bug, converted string to int

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -5,7 +5,7 @@ wave_obj = sa.WaveObject.from_wave_file(filepath)
 play_obj = wave_obj.play()
 play_obj.wait_done()
 
-n_times = input("How often do you want the file to play (int)?")
+n_times = int(input("How often do you want the file to play (int)?"))
 
 for x in range(n_times):
     play_obj.wait_done()
