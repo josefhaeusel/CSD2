4409fd763a22130daa2e6cce303c28a52d58271b
Josef Haeusel <josef.haeusel@yahoo.de>
2023-09-08 19:03:04 +0200

    modified playsound.py and added sample toycar.wav

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,11 +1,9 @@
 import simpleaudio as sa
 
-filepath = input("input path to .wav file")
+filepath = "/Users/josef.haeusel/My Drive/Musikdesign/HKU/Unterricht/Python/CSD2_git/python_basics/toycar.wav"  #input("input path of .wav file")
 wave_obj = sa.WaveObject.from_wave_file(filepath)
-play_obj = wave_obj.play()
-play_obj.wait_done()
-
 n_times = int(input("How often do you want the file to play (int)?"))
 
 for x in range(n_times):
+    play_obj = wave_obj.play()
     play_obj.wait_done()
