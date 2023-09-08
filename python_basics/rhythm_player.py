116af2ef122358a984af8c2b5ebe38d68cadc0e5
Josef Haeusel <josef.haeusel@yahoo.de>
2023-09-08 19:07:03 +0200

    updated playsound.py

--- python_basics/rhythm_player.py
+++ python_basics/rhythm_player.py
@@ -1,6 +1,6 @@
 import simpleaudio as sa
 
-filepath = input("input path of .wav file")
+filepath = "/Users/josef.haeusel/My Drive/Musikdesign/HKU/Unterricht/Python/CSD2_git/python_basics/toycar.wav"  #input("input path of .wav file")
 wave_obj = sa.WaveObject.from_wave_file(filepath)
 n_times = int(input("How often do you want the file to play (int)?"))
 
