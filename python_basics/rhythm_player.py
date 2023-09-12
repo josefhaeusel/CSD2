a56e3354b23ac550a1f4b5bca75178aacc5d702b
Josef Haeusel <josef.haeusel@yahoo.de>
2023-09-12 12:32:34 +0200

    Updated rhythm_player.py

--- python_basics/rhythm_player.py
+++ python_basics/rhythm_player.py
@@ -1,19 +1,22 @@
 import simpleaudio as sa
 import time
 
-filepath = "/Users/josef.haeusel/My Drive/Musikdesign/HKU/Unterricht/Python/CSD2_git/python_basics/toycar.wav"  #OPTIONAL: input("input path of .wav file")
+filepath = "/Users/josef.haeusel/My Drive/Musikdesign/HKU/Unterricht/Python/CSD2_git/python_basics/toycar.wav"
 wave_obj = sa.WaveObject.from_wave_file(filepath)
 
-numPlaybackTimes = int(input("How often do you want the file to play (int)?"))
+numPlaybackTimes = int(input("How often do you want the file to play (int)?\n"))
 playback_rhythms = []
 
 for x in range(numPlaybackTimes):
-    rhythm_value = [float(input("\nHow long is the {}. note of the {}?".format(x+1, numPlaybackTimes)))]
+    rhythm_value = [float(input("\nHow long is the {}. note of the {}?\n".format(x+1, numPlaybackTimes)))]
     playback_rhythms = playback_rhythms + rhythm_value
     print("-> Your rhythm:", playback_rhythms)
 
-playback_bpm = 60/int(input("\nAt how many BPM should your rhythm be played?"))
+playback_bpm = 60/int(input("\nAt how many BPM should your rhythm be played?\n"))
 
 for x in range(numPlaybackTimes):
+    print("Playing note {}".format(x+1))
     play_obj = wave_obj.play()
     time.sleep(playback_rhythms[x]*playback_bpm)
+    
+    
