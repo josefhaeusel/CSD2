6983efd9441c493ebd5d0a8978516c8321dcf3b5
Josef Haeusel <josef.haeusel@yahoo.de>
2023-09-22 16:38:30 +0200

    Updated Rhythm_Player 16th Notes

--- CSD2a/opdrachten/python_basics/rhythm_player_16th_notes.py
+++ CSD2a/opdrachten/python_basics/rhythm_player_16th_notes.py
@@ -2,11 +2,23 @@ import simpleaudio as sa
 import time
 import librosa
 
-FILEPATH = "/Users/josef.haeusel/My Drive/Musikdesign/HKU/Unterricht/Python/CSD2_git/CSD2a/opdrachten/python_basics/toycar.wav"
+FILEPATH = "/Users/josef.haeusel/My Drive/Musikdesign/HKU/Unterricht/Python/CSD2_git/CSD2a/opdrachten/python_basics/toycar.wav"   
 
+def errorFunctionNumber(number, check_int = True, check_positive = True):
+    
+    if check_positive and number <= 0:
+        print(f"ERROR: Number has to be positive")
+        return "Error"
+
+##Try Catch      
 
 def inputNoteDurations():
-    numPlaybackTimes = int(input("How often do you want the sample to play (int)?   "))
+    while True:
+      numPlaybackTimes = int(input("How often do you want the sample to play (int)?   "))
+      if errorFunctionNumber(numPlaybackTimes, check_positive=True) == "Error":
+         continue
+      else:
+         break
 
     print("\n0.25 = Sixteenth Note, 1 = Quarter Note, 2 = Half Note,... ")
     noteDurations = []
@@ -33,6 +45,7 @@ def sixteenthTimestampsToSecondTimestamps(sixteenthTimestamps, bpm):
 
 def input_bpm(default_bpm):
   bpm_input = input(f"Set BPM or press Enter for default ({default_bpm:.2f} BPM):   ")
+
   if bpm_input == "":
     return default_bpm
   else:
@@ -53,11 +66,14 @@ def playbackTimestamps(timestampsSeconds, filepath):
         elapsed_time = time.time() - start_time
         if elapsed_time < timestamp:
             time.sleep(timestamp - elapsed_time)
-        elapsed_time = time.time() - start_time
+            elapsed_time = time.time() - start_time
 
-        print(f"Playing note {note+1} in {bpm} BPM. Elapsed time: {elapsed_time}")
+        print(f"Playing note {note+1}. Elapsed time: {elapsed_time}")
+
         if note+1 < len(timestampsSeconds):
+        
             play_obj = wave_obj.play()
+
         else:
             play_obj = wave_obj.play()
             time.sleep(sample_duration)
@@ -65,8 +81,52 @@ def playbackTimestamps(timestampsSeconds, filepath):
     print("\nPlayback finished.\n")
 
 
-noteDurations = inputNoteDurations()
-bpm = input_bpm(default_bpm=60)
-timestamps16th = durationsToSixteenthTimestamps(noteDurations)
-timestampsSeconds = sixteenthTimestampsToSecondTimestamps(timestamps16th, bpm)
-playbackTimestamps(timestampsSeconds, FILEPATH)
+
+###Ciskas Solution with while Loop.
+def playbackTimestamps2(timestampsSeconds, filepath):
+
+    print("\n\nTIMESTAMPS", timestampsSeconds, "\n\n")
+    y, sr = librosa.load(filepath)
+    sample_duration = librosa.get_duration(y=y, sr=sr)
+    wave_obj = sa.WaveObject.from_wave_file(filepath)
+
+
+    if timestampsSeconds:
+      timestamp = timestampsSeconds.pop(0)
+    else:
+       exit()
+
+    start_time = time.time()
+    is_playing = True
+    while is_playing:
+        elapsed_time = time.time() - start_time
+        
+        if elapsed_time >= timestamp:
+          play_obj = wave_obj.play()
+          print(f"Playing note at {timestamp} in BPM. Elapsed time: {elapsed_time}")
+          print(timestampsSeconds)
+          timestampsSeconds.pop(0)
+        else:
+           time.sleep(0.0001)
+        
+        if len(timestampsSeconds) == 1:
+          play_obj = wave_obj.play()
+          time.sleep(sample_duration)
+        
+
+          
+
+    print("\nPlayback finished.\n")
+
+
+def main():
+  noteDurations = inputNoteDurations()
+  bpm = input_bpm(default_bpm=60)
+  timestamps16th = durationsToSixteenthTimestamps(noteDurations)
+  timestampsSeconds = sixteenthTimestampsToSecondTimestamps(timestamps16th, bpm)
+  playbackTimestamps(timestampsSeconds, FILEPATH)
+
+if __name__ == "__main__":
+  main()
+
+
